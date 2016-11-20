2be7a39fe2e79b16ee53eb6ff7318cae4104fb62
Dave Bennett <dnbennett@hotmail.co.uk>
2016-11-20 23:00:23 +0000

    Projects added. Navbar fadeIn added.

--- scripts/custom.js
+++ scripts/custom.js
@@ -2,3 +2,15 @@ function dropLinks() {
     document.getElementById("navDropdown").classList.toggle("show");
 }
 
+var project = document.querySelector("#myProjects");
+
+window.onscroll = function() {
+    var position = project.getBoundingClientRect();
+    var top = position.top - 120;
+    var navbar = document.getElementsByTagName("nav")[0]
+    if (top < 0) {
+        navbar.style.backgroundColor = "rgba(0, 0, 0, " + Math.abs(top / 100) + ")";
+    };
+}
+
+window.onscroll();
