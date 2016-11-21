5df3dbf16cea0d2d74e1c3736a9dd250f0149150
Dave Bennett <dnbennett@hotmail.co.uk>
2016-11-21 22:47:39 +0000

    Navbar opacity changed

--- scripts/custom.js
+++ scripts/custom.js
@@ -4,7 +4,7 @@ window.onscroll = function() {
     var position = project.getBoundingClientRect();
     var top = position.top - 120;
     var navbar = document.getElementsByTagName("nav")[0]
-    if (top < 0) {
+    if (top < 0 && Math.abs(top / 100) < 0.8) {
         navbar.style.backgroundColor = "rgba(0, 0, 0, " + Math.abs(top / 100) + ")";
     };
 }
