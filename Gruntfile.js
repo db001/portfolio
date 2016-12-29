fb3c9cf7dbff837cf34d904de22aa8baf6427a10
db001 <dnbennett@hotmail.co.uk>
2016-12-29 22:03:25 +0000

    gitignore updated to include node_modules and .sass-cache

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,5 @@
 module.exports = function(grunt) {
   grunt.initConfig({
-<<<<<<< HEAD
 
     watch: {
       sass: {
@@ -17,13 +16,6 @@ module.exports = function(grunt) {
         files: {
           // destination          // source file
           "css/styles.css" : "scss/styles.scss"
-=======
-    sass: {
-      dist: {
-        files: {
-          // destination          // source file
-          "app/css/styles.css" : "app/scss/styles.scss"
->>>>>>> 5d9eb5bc95a75aff088e53e94007eca41424e9f5
         }
       }
     },
@@ -32,11 +24,7 @@ module.exports = function(grunt) {
       default_options: {
         bsFiles: {
           src: [
-<<<<<<< HEAD
             "css/*.css",
-=======
-            "app/css/*.css",
->>>>>>> 5d9eb5bc95a75aff088e53e94007eca41424e9f5
             "*.html"
           ]
         },
@@ -53,13 +41,8 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-browser-sync');
-<<<<<<< HEAD
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.registerTask('default', ['sass', 'browserSync', 'watch']);
-=======
-  grunt.registerTask('default', ['sass', 'browserSync']);
->>>>>>> 5d9eb5bc95a75aff088e53e94007eca41424e9f5
-
 };
 
 // browserSync localhost:3000
