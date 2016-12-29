fa36dbac9e4758ef6700e6c69de3992dd31cd6ba
db001 <dnbennett@hotmail.co.uk>
2016-12-29 21:57:06 +0000

    Gruntfile fixed - browser-sync and grunt-contrib-watch now working

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+
+    watch: {
+      sass: {
+        files: "app/scss/*.scss",
+        tasks: ['sass']
+      }
+    },
+
+    sass: {
+      dist: {
+        options: {
+          sourcemap: 'none'
+        },
+        files: {
+          // destination          // source file
+          "css/styles.css" : "scss/styles.scss"
+        }
+      }
+    },
+
+    browserSync: {
+      default_options: {
+        bsFiles: {
+          src: [
+            "css/*.css",
+            "*.html"
+          ]
+        },
+        options: {
+          watchTask: true,
+          server: {
+            baseDir: "./"
+          }
+        }
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default', ['sass', 'browserSync', 'watch']);
+
+};
+
+// browserSync localhost:3000
